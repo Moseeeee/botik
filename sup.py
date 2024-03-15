fb88ec5be1a9f0f191a6b70cf1caace6430b1e10
Moseeeee <104442796+Moseeeee@users.noreply.github.com>
2024-03-15 17:08:26 +0300

    Update sup.py

--- sup.py
+++ sup.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import BotCommand, BotCommandScopeChat
